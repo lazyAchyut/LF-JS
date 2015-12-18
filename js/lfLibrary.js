99b6a1ec55a69d0d2f943af44875ba270c1c846a
Achyut Pokhrel <achyut.pkl@gmail.com>
2015-12-18 18:29:05 +0545

    lf-repeate service refactored

--- js/lfLibrary.js
+++ js/lfLibrary.js
@@ -1,8 +1,10 @@
 ;(function(window, document, undefined) {
-'use strict'
+'use strict';
+
 function Main(){
 	var $routeObj;
 	var $dataObj;
+	var $repeatObj;
 	var $scope = {};
 	this.$rootElement = [];
 	var that = this;
@@ -11,15 +13,18 @@ function Main(){
 	$scope = {model1:'Kiran'};
 	$scope.user = [{
 		name : 'Achyut Pokhrel',
-		address : 'Dang'
+		address : 'Dang',
+		link : 'http://localhost/myfiles/prototype/#/user/1'
 	},
 	{
 		name : 'Kiran Kharel',
-		address : 'Jhapa'
+		address : 'Jhapa',
+		link : 'http://localhost/myfiles/prototype/#/user/2'
 	},
 	{
 		name : 'Gaurab KC',
-		address : 'Kathmandu'
+		address : 'Kathmandu',
+		link : 'http://localhost/myfiles/prototype/#/user/3'
 	}
 	]; 
 
@@ -37,8 +42,16 @@ function Main(){
 		$dataObj.$registerWatcher();
 		$dataObj.$addListener();
 		$dataObj.$initializeFirstView();
+	}	
+
+	this.$initializeRepeat = function(){
+		$repeatObj = new LfRepeat();
+		$repeatObj.$doRepeat();
+		//after lf-repeate, new links may be added
+		$routeObj.$addListener();
 	}
 
+	//invoke updateview method inside LfData class from Object.Observe section 
 	this.$invokeUpdateView = function(tag){
 		$dataObj.$updateView(tag);
 	}
@@ -47,16 +60,19 @@ function Main(){
 		$scope[key] = value;
 	}
 
-	this.getScope = function(){
+	this.$getScope = function(){
 		return $scope;
 	}
 
 } //end of Main Class
 
 
+//create instance of Main class
 var $main = new Main();
-$main.$initializeRoute();
+if(typeof $routeProvider === "function")	//invoke only if routing is defined by user in myapp.js
+	$main.$initializeRoute();
 $main.$initializeDataBind();
+$main.$initializeRepeat();
 
 
 
@@ -88,13 +104,13 @@ function LfBind(){
 	this.$initializeFirstView = function(){
 		for(var i = 0, len = $watchModels.length; i < len; i++){
 			var $tag = $watchModels[i].getAttribute('lf-model'); 	
-			if($main.getScope().hasOwnProperty($tag))
+			if($main.$getScope().hasOwnProperty($tag))
 				that.$updateView($tag);
 		}
 
 		for(var i = 0, len = $watchBinds.length; i < len; i++){
 			var $tag = $watchBinds[i].getAttribute('lf-bind'); 	
-			if($main.getScope().hasOwnProperty($tag))
+			if($main.$getScope().hasOwnProperty($tag))
 				that.$updateView($tag);
 		}
 	}	
@@ -103,15 +119,15 @@ function LfBind(){
 	this.$updateView = function($tag){
 	   	for(var j = 0, len = $watchModels.length; j < len; j++){
 			if($tag === $watchModels[j].getAttribute('lf-model')){
-				$watchModels[j].value = $main.getScope()[$tag];
-				$watchModels[j].innerHTML = $main.getScope()[$tag];
+				$watchModels[j].value = $main.$getScope()[$tag];
+				$watchModels[j].innerHTML = $main.$getScope()[$tag];
 			}
     	} 
 
     	for(var j = 0, len = $watchBinds.length; j < len; j++){
 			if($tag === $watchBinds[j].getAttribute('lf-bind')){
-				$watchBinds[j].value = $main.getScope()[$tag];
-				$watchBinds[j].innerHTML = $main.getScope()[$tag];
+				$watchBinds[j].value = $main.$getScope()[$tag];
+				$watchBinds[j].innerHTML = $main.$getScope()[$tag];
 			}
     	}   	
 	}
@@ -120,27 +136,25 @@ function LfBind(){
 
 
 //observe changes in $scope object, if any change is detected it updates respective models and bind
-Object.observe($main.getScope(), function(changes){
+Object.observe($main.$getScope(), function(changes){
     changes.forEach(function(change) {
     	// console.log(change.type, ' : ',change.name,' : ', change.oldValue);
     	$main.$invokeUpdateView(change.name);
-
    	 }); //end of change.foreach
 }); //end of object.observe
 
 
 //routing section
 function LfRoute(){
-		var $userDefinedRoutes = {};
-		var $allLinks = [];
-		var $xhr;
+		const $USER_DEFINED_ROUTES = $routeProvider(); //$routeProvider() returns user defined routes, lies in user's controller
+		var $allLinks;
+		var $xhr; //XMLHttpRequest object
 		var that = this;
 
-		$userDefinedRoutes = $routeProvider(); //$routeProvider() returns user defined routes, lies in user's controller
-		$allLinks = $main.$rootElement.querySelectorAll('a');
-
+		//get all link and when click event is detected call $doRoute method	
 	    this.$addListener = function(){ 
-	    	//get all link and when click event is detected call $doRoute method	
+	    	//after rendering first view, lf-repeate might rendor some dynamic links later so, it needs to be rechecked again and again	
+	    	$allLinks = $main.$rootElement.querySelectorAll('a');
 			for(var i = 0, len = $allLinks.length; i < len; i++){
 				$allLinks[i].addEventListener('click',function(evt){
 					setTimeout(that.$doRoute, 0);
@@ -169,7 +183,7 @@ function LfRoute(){
 					that.$loadView($container, $path);
 				}
 				else{ 
-					var $redirectPath = that.$getOtherwisePath($userDefinedRoutes);
+					var $redirectPath = that.$getOtherwisePath($USER_DEFINED_ROUTES);
 					if($redirectPath != null ){
 						window.location.href = $parsedUrl[0] + '#' + $redirectPath;  //logout
 						$matchedRoute = that.$mapUrlAfterHash($redirectPath); 
@@ -186,35 +200,35 @@ function LfRoute(){
 
 		//returns matched route's path and templateUrl
 		this.$mapUrlAfterHash = function($path){ 
-			var tempParsedPath = [];
-			tempParsedPath = that.$parseStr($path , '/'); 
-			for(var i = 0; i< $userDefinedRoutes.length-1;i++){  //dont check otherwise part in $userDefinedRoutes
-				var $tempJson = that.$parseStr($userDefinedRoutes[i].when , '/');
+			var $tempParsedPath = [];
+			$tempParsedPath = that.$parseStr($path , '/'); 
+			for(var i = 0; i< $USER_DEFINED_ROUTES.length-1;i++){  //dont check otherwise part in $USER_DEFINED_ROUTES
+				var $tempJson = that.$parseStr($USER_DEFINED_ROUTES[i].when , '/');
 				var flag = true;
-				if(tempParsedPath.length === $tempJson.length)
+				if($tempParsedPath.length === $tempJson.length)
 				{
-					for(var j=0; j<tempParsedPath.length;j++)
+					for(var j=0; j<$tempParsedPath.length;j++)
 					{
-						if(tempParsedPath[j] === $tempJson[j])
+						if($tempParsedPath[j] === $tempJson[j])
 							continue;
 						else if($tempJson[j].substr(0,1)===':')
-							console.log("Assign " + tempParsedPath[j] + " to " +  $tempJson[j] );
+							console.log("Assign " + $tempParsedPath[j] + " to " +  $tempJson[j] );
 						else{
 							flag = false;
 							break;
 						}
 					}
 					if(flag === true)
-						return $userDefinedRoutes[i];
+						return $USER_DEFINED_ROUTES[i];
 				}
 			}
 		}
 
 		//returns redirectTO url of otherwise property in user defined route
 		this.$getOtherwisePath = function(){
-			for(var i in $userDefinedRoutes){ 
-				if($userDefinedRoutes[i].otherwise == '')
-					return $userDefinedRoutes[i].redirectTo;
+			for(var i in $USER_DEFINED_ROUTES){ 
+				if($USER_DEFINED_ROUTES[i].otherwise == '')
+					return $USER_DEFINED_ROUTES[i].redirectTo;
 			}
 		}
 
@@ -245,33 +259,46 @@ function LfRoute(){
 
 
 //lf-repeate
-var $repeatService = function(){
-	var $allRepeats = $rootElement.querySelectorAll('[lf-repeat]');
-	for(var i=0;i<$allRepeats.length;i++){ 
-		var $repeatAttribute = $allRepeats[i].getAttribute('lf-repeat'); 
-		$repeatAttribute = $repeatAttribute.trim(); 
-		if($scope.hasOwnProperty($repeatAttribute)){
-			var $innerBinds = $allRepeats[i].querySelectorAll('[lf-bind]');
-			for(var j=0;j<$scope[$repeatAttribute].length;j++){
-				for(var noOfBinds=0;noOfBinds<$innerBinds.length;noOfBinds++){
-					var $bindAttr = $innerBinds[noOfBinds].getAttribute('lf-bind');
-					$bindAttr = $bindAttr.trim();
-					if($scope[$repeatAttribute][j].hasOwnProperty($bindAttr)){
-						var temp = document.createElement($innerBinds[noOfBinds].tagName);
-						if($innerBinds[noOfBinds].tagName === "a"){
+function LfRepeat(){
+	const $ALL_REPEATS = $main.$rootElement.querySelectorAll('[lf-repeat]');
+	var $currentRepeat; 
+	var $collectionName; //scope property
+	var $bindAttr; //nested scope property
+	var that = this;
 
-						}
-						else
-							var text = document.createTextNode($scope[$repeatAttribute][j][$bindAttr]);  
-						temp.appendChild(text);
-						$allRepeats[i].appendChild(temp);
-
+	this.$doRepeat = function(){
+		for(var i = 0; i<$ALL_REPEATS.length;i++){
+			$currentRepeat = $ALL_REPEATS[i];
+			$collectionName = ($currentRepeat.getAttribute('lf-repeat')).trim();
+			
+			if($main.$getScope().hasOwnProperty($collectionName)){
+				var $innerBinds = $currentRepeat.querySelectorAll('[lf-bind]');
+				that.$removeElements($currentRepeat,$innerBinds);
+				for(var j=0;j<$main.$getScope()[$collectionName].length;j++){
+					for(var noOfBinds=0;noOfBinds<$innerBinds.length;noOfBinds++){
+						$bindAttr = ($innerBinds[noOfBinds].getAttribute('lf-bind')).trim();
+						if($main.$getScope()[$collectionName][j].hasOwnProperty($bindAttr))
+							that.$renderRepeat($innerBinds[noOfBinds],j);
 					}
 				}
 			}
 		}
 	}
-}
+
+	this.$removeElements = function(parent,child){
+		for(var i=0;i<child.length;i++)
+			parent.removeChild(child[i]);
+	}
+
+	this.$renderRepeat = function(element,index){
+		element = element.cloneNode();
+		element.removeAttribute('lf-bind');
+		element.innerHTML = $main.$getScope()[$collectionName][index][$bindAttr];
+		$currentRepeat.appendChild(element);
+		$currentRepeat.innerHTML = $currentRepeat.innerHTML.replace('{{'+$bindAttr+'}}',$main.$getScope()[$collectionName][index][$bindAttr]);
+	}
+	
+} //end of class LfRepeat
 
 
 
